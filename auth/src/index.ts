83831c9debe594d24f9e34c6c0d4c12c7f19758b
abdomohamed96 <abdomohamed9192@gmail.com>
2025-06-25 19:00:31 +0300

    Dummy change

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import {app} from "./app";
 
 const start = async () => {
-    console.log('Starting Server.....')
+    console.log('Starting.....')
     if(!process.env.JWT_KEY){
         throw new Error('JWT_KEY must be defined')
     }
